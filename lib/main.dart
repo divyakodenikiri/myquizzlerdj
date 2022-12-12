0858d28e6d3210eee0543a37da1bdd1015715f8b
divyakodenikiri <divyapadpu99@gmail.com>
2022-12-12 11:11:05 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -24,28 +24,14 @@ class QuizPage extends StatefulWidget {
   _QuizPageState createState() => _QuizPageState();
 }
  class _QuizPageState extends State<QuizPage> {
-  List<Icon> scorekeeper = [
-     Icon(
-          Icons.check,
-          color: Colors.blue,
-        ),
-         Icon(
-          Icons.close,
-          color: Colors.red,
-        ),
-         Icon(
-          Icons.close,
-          color: Colors.blue,
-        ),
-         Icon(
-          Icons.close,
-          color: Colors.blue,
-        ),
-         Icon(
-          Icons.close,
-          color: Colors.blue,
-        ),
+  List<Icon> scorekeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
+
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -58,7 +44,7 @@ class QuizPage extends StatefulWidget {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -84,11 +70,9 @@ class QuizPage extends StatefulWidget {
               onPressed: () {
                 //The user picked true.
                 setState(() {
-                   scorekeeper.add(
-                  Icon(Icons.check, color: Colors.green,),
-                );
+                   questionNumber++;
                 });
-               
+                print(questionNumber);
               },
             ),
           ),
@@ -107,6 +91,10 @@ class QuizPage extends StatefulWidget {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  questionNumber++;
+                });
+                print(questionNumber);
               },
             ),
           ),
@@ -118,7 +106,8 @@ class QuizPage extends StatefulWidget {
     );
   }
 }
-
+// hi everyone
+//hi good morning
 
 
 /*
